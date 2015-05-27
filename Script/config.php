b3ec381b48946979f4fd1424c70f53c520892890
Pascal Drewes <pascal@drewes-webdesign.nl>
2015-05-27 09:41:04 +0200

    PDO scriptjes

---
+++ Script/config.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Configuration for the DB
+ */
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "nethero");
+define("DB_USER", "bytebusters");
+define("DB_PASS", "Stenden2015");
+define("DB_TABLE", "score");
+
+/*
+ * Main configuration
+ */
+
+define("DEBUG", true);
